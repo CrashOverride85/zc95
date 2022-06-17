239c4eca3f1dacf0085593ee30efb2fb3008b12d
CrashOverride85 <crashoverride85@outlook.com>
2022-06-17 11:48:58 +0100

    increase time required to end game

--- source/zc95/core1/routines/CBuzz.cpp
+++ source/zc95/core1/routines/CBuzz.cpp
@@ -173,7 +173,7 @@ void CBuzz::trigger(trigger_socket socket, trigger_part part, bool active)
         {
             if (!_end_game_at_us)
             {
-                const int delay_ms = 25;
+                const int delay_ms = 100;
                 _end_game_at_us = time_us_64() + (1000 * delay_ms);
             }
         }
