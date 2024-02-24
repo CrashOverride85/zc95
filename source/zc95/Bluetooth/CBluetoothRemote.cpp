505835f73f82e55010f9b34ba5d70170428e8146
CrashOverride85 <crashoverride85@outlook.com>
2024-02-24 01:44:12 +0000

    can read up/down/left/right from remotes

--- source/zc95/Bluetooth/CBluetoothRemote.cpp
+++ source/zc95/Bluetooth/CBluetoothRemote.cpp
@@ -3,7 +3,7 @@
 CBluetoothRemote::CBluetoothRemote()
 {
     printf("CBluetoothRemote()\n");
-    _pressed = false;
+    _movement_started = false;
 }
 
 CBluetoothRemote::~CBluetoothRemote()
@@ -11,72 +11,116 @@ CBluetoothRemote::~CBluetoothRemote()
     printf("~CBluetoothRemote()\n");
 }
 
+void CBluetoothRemote::reset_dimension(dimension_t &dimension)
+{
+    dimension.prev_val = 0;
+    dimension.most_recent = 0;
+    dimension.received_count = 0;
+    dimension.initial_val = 0;
+}
 
 void CBluetoothRemote::process_input(uint16_t usage_page, uint16_t usage, int32_t value)
 {
+    bool debug = true;
+
     switch(usage_page)
     {
         case HID_USAGE_PAGE_GENERIC_DESKTOP:
+            if (debug) print_desktop_page(usage, value);
             process_desktop_page(usage, value);
             break;
 
         case HID_USAGE_PAGE_BUTTON:
+            if (debug) print_button_page(usage, value);
             process_button_page(usage, value);
             break;
 
         case HID_USAGE_PAGE_DIGITIZER:
+            if (debug) print_digitizer_page(usage, value);
             process_digitizer_page(usage, value);
             break;
 
+        case HID_USAGE_PAGE_CONSUMER:
+            if (debug) print_consumer_page(usage, value);
+            process_consumer_page(usage, value);
+            break;
+
         default:
-            printf("CBluetoothRemote::process_input(): Unexpected usage page: %d\n", usage_page);
+            printf("CBluetoothRemote::process_input(): Unexpected usage page: 0x%X\n", usage_page);
             break;
     }
 
+
+    if (is_movement_start(usage_page, usage, value))
+    {
+        _movement_started = true;
+
+        _x.initial_val = _x.most_recent;
+        _y.initial_val = _y.most_recent;
+        
+        _x.received_count = 0;
+        _y.received_count = 0;
+    }
+    else if (is_movement_end(usage_page, usage, value))
+    {
+        _movement_started = false;
+        keypress_t key = get_last_button_pressed();
+        print_keypress(key);
+        printf("\n");
+    }
+
+}
+
+
+bool CBluetoothRemote::is_movement_start(uint16_t usage_page, uint16_t usage, int32_t value)
+{
+    if (_movement_started)
+        return false;
+
+    return (value == 1 && is_movement_event(usage_page, usage));
+}
+
+bool CBluetoothRemote::is_movement_end(uint16_t usage_page, uint16_t usage, int32_t value)
+{
+    if (!_movement_started)
+        return false;
+
+    return (value == 0 && is_movement_event(usage_page, usage));
+}
+
+// Different buttons seem to use different things to indicate the start/end of a left/right/up/down swipe,
+// and some send more than one.
+// So look for various different things.
+bool CBluetoothRemote::is_movement_event(uint16_t usage_page, uint16_t usage)
+{
+    if (usage_page == HID_USAGE_PAGE_BUTTON     && usage == HID_USAGE_BUTTON_PRIMARY        )  return true;
+    if (usage_page == HID_USAGE_PAGE_DIGITIZER  && usage == HID_USAGE_DIGITIZER_TIP_SWITCH  )  return true;
+    if (usage_page == HID_USAGE_PAGE_DIGITIZER  && usage == HID_USAGE_DIGITIZER_IN_RANGE    )  return true;
+
+    return false;
 }
 
 void CBluetoothRemote::process_desktop_page(uint16_t usage, int32_t value)
 {
-    switch(usage)
-    {
-        case HID_USAGE_GENERIC_DESKTOP_X:
-            printf("HID_USAGE_GENERIC_DESKTOP_X:            %ld\n", value);
-            break;
+ //   if (!_movement_started)
+ //       return;
 
-        case HID_USAGE_GENERIC_DESKTOP_Y:
-            printf("HID_USAGE_GENERIC_DESKTOP_Y:            %ld\n", value);
-            break;
-
-        default:
-            printf("generic desktop: %x = %ld\n", usage, value);
-            break;
-    }
-
-/*
     if (usage == HID_USAGE_GENERIC_DESKTOP_X)
     {
-        _current_x = value;
+        _x.received_count++;
+        _x.prev_val = _x.most_recent;
+        _x.most_recent = value;
     }
     else if (usage == HID_USAGE_GENERIC_DESKTOP_Y)
     {
-        _current_y = value;
+        _y.received_count++;
+        _y.prev_val = _y.most_recent;
+        _y.most_recent = value;
     }
-    */
 }
 
 void CBluetoothRemote::process_button_page(uint16_t usage, int32_t value)
 {
-
-    switch(usage)
-    {
-        case HID_USAGE_BUTTON_PRIMARY:
-            printf("HID_USAGE_BUTTON_PRIMARY:               %ld\n", value);
-            break;
-
-        default:
-            printf("button page: %x = %ld\n", usage, value);
-            break;
-    }
 
     /*
     if (usage == HID_USAGE_BUTTON_PRIMARY)
@@ -98,32 +142,77 @@ void CBluetoothRemote::process_button_page(uint16_t usage, int32_t value)
 
 void CBluetoothRemote::process_digitizer_page(uint16_t usage, int32_t value)
 {
-    switch(usage)
+
+}
+
+void CBluetoothRemote::process_consumer_page(uint16_t usage, int32_t value)
+{
+   
+}
+
+
+CBluetoothRemote::keypress_t CBluetoothRemote::get_last_button_pressed()
+{
+    int32_t x_move = 0;
+    int32_t y_move = 0;
+
+    if (_x.received_count < 2 && _y.received_count < 2 )
+        return keypress_t::KEY_NONE;
+
+
+    // For remotes that are doing something sensible: movement start, 2 
+    // or more x/y updates (hopefully with only one changing), movement end.
+    // This works well for 3 of 4 remotes with up/down buttons I have.
+    x_move = _x.most_recent - _x.prev_val;
+    y_move = _y.most_recent - _y.prev_val;
+
+    if (y_move == 0 && x_move == 0)
     {
-        case HID_USAGE_DIGITIZER_IN_RANGE:
-            printf("HID_USAGE_DIGITIZER_IN_RANGE:           %ld\n", value);
-            break;
+        // A bit of weird case - we've received at least 2 x/y updates, but there was no apparent movement based
+        // off those two x/y's received.  Take a crude guess at what button was pressed - if only one (x or y) is 
+        // non-zero, assume that's the direction of movement.
+        // Applies to 1 of the 4 remotes I have with up/down buttons
 
-        case HID_USAGE_DIGITIZER_TIP_SWITCH:
-            printf("HID_USAGE_DIGITIZER_TIP_SWITCH:         %ld\n", value);
-            break;
+        if ((_x.most_recent == 0 && _y.most_recent == 0) || 
+            (_x.most_recent != 0 && _y.most_recent != 0))
+            return keypress_t::KEY_NONE; // give up
 
-        case HID_USAGE_DIGITIZER_CONTACT_IDENTIFIER:
-            printf("HID_USAGE_DIGITIZER_CONTACT_IDENTIFIER: %ld\n", value);
-            break;
+        if (_x.most_recent > 0)
+            return keypress_t::KEY_LEFT;
+        else if (_x.most_recent < 0)
+            return keypress_t::KEY_RIGHT;
+        else if (_y.most_recent > 0)
+            return keypress_t::KEY_UP;
+        else if (_y.most_recent < 0)
+            return keypress_t::KEY_DOWN;
+        else
+            return keypress_t::KEY_NONE;
+    }
 
-        case HID_USAGE_DIGITIZER_CONTACT_COUNT:
-            printf("HID_USAGE_DIGITIZER_CONTACT_COUNT:      %ld\n", value);
-            break;
-
-        default:
-            printf("digitizer: %x = %ld\n", usage, value);
-            break;
+    // Try and determine which direction the button is trying to move in most. 
+    // Ideally, either X or Y move would be 0 making it obvious.
+    if (abs(x_move) > abs(y_move))
+    {
+        // largest movement in x => left/right
+        if (x_move < 0)
+            return keypress_t::KEY_LEFT;
+        else
+            return keypress_t::KEY_RIGHT;
+    }
+    else
+    {
+        // largest movement in y => up/down
+        if (y_move < 0)
+            return keypress_t::KEY_UP;
+        else
+            return keypress_t::KEY_DOWN;
     }
 }
 
 void CBluetoothRemote::end_of_input()
 {
+
+    /*
     bool location_updated = false;
     int x_diff = 0;
     int y_diff = 0;
@@ -154,6 +243,7 @@ void CBluetoothRemote::end_of_input()
 
     if (location_updated)
         printf("\t\t%li\t%li\t\t\t(%d\t%d)\n", _last_x, _last_y, x_diff, y_diff);
+        */
 }
 
 void CBluetoothRemote::report_keypress()
@@ -177,8 +267,171 @@ void CBluetoothRemote::report_keypress()
             break;
     }
 
-    if (_pressed)
+    if (_movement_started)
         printf("(pressed)\n");
     else
         printf("(released)\n");
+}
+
+
+//////////////////
+// Debug output //
+//////////////////
+
+void CBluetoothRemote::print_keypress(keypress_t key)
+{
+    switch(key)
+    {
+        case keypress_t::KEY_NONE:
+            printf("NONE");
+            break;
+
+        case keypress_t::KEY_UP:
+            printf("KEY_UP");
+            break;
+
+        case keypress_t::KEY_DOWN:
+            printf("KEY_DOWN");
+            break;
+
+        case keypress_t::KEY_LEFT:
+            printf("KEY_LEFT");
+            break;
+
+        case keypress_t::KEY_RIGHT:
+            printf("KEY_RIGHT");
+            break;
+
+        case keypress_t::KEY_OK:
+            printf("KEY_OK");
+            break;
+
+        case keypress_t::KEY_SHUTTER:
+            printf("KEY_SHUTTER");
+            break;            
+    }
+}
+
+void CBluetoothRemote::print_desktop_page(uint16_t usage, int32_t value)
+{
+    switch(usage)
+    {
+        case HID_USAGE_GENERIC_DESKTOP_X:
+            printf("HID_USAGE_GENERIC_DESKTOP_X:            %ld\n", value);
+            break;
+
+        case HID_USAGE_GENERIC_DESKTOP_Y:
+            printf("HID_USAGE_GENERIC_DESKTOP_Y:            %ld\n", value);
+            break;
+
+        case HID_USAGE_GENERIC_DESKTOP_WHEEL:
+            printf("HID_USAGE_GENERIC_DESKTOP_WHEEL:        %ld\n", value);
+            break;
+
+        default:
+            printf("generic desktop: 0x%X = %ld\n", usage, value);
+            break;
+    }
+}
+
+void CBluetoothRemote::print_button_page(uint16_t usage, int32_t value)
+{
+    switch(usage)
+    {
+        case HID_USAGE_BUTTON_PRIMARY:
+            printf("HID_USAGE_BUTTON_PRIMARY:               %ld\n", value);
+            break;
+
+        default:
+            printf("button page:                            0x%X = %ld\n", usage, value);
+            break;
+    }
+}
+
+void CBluetoothRemote::print_digitizer_page(uint16_t usage, int32_t value)
+{
+    switch(usage)
+    {
+        case HID_USAGE_DIGITIZER_IN_RANGE:
+            printf("HID_USAGE_DIGITIZER_IN_RANGE:           %ld\n", value);
+            break;
+
+        case HID_USAGE_DIGITIZER_TIP_SWITCH:
+            printf("HID_USAGE_DIGITIZER_TIP_SWITCH:         %ld\n", value);
+            break;
+
+        case HID_USAGE_DIGITIZER_CONTACT_IDENTIFIER:
+            printf("HID_USAGE_DIGITIZER_CONTACT_IDENTIFIER: %ld\n", value);
+            break;
+
+        case HID_USAGE_DIGITIZER_CONTACT_COUNT:
+            printf("HID_USAGE_DIGITIZER_CONTACT_COUNT:      %ld\n", value);
+            break;
+
+        default:
+            printf("digitizer: 0x%X = %ld\n", usage, value);
+            break;
+    }
+}
+
+void CBluetoothRemote::print_consumer_page(uint16_t usage, int32_t value)
+{
+    switch(usage)
+    {
+        case HID_USAGE_CONSUMER_UNDEFINED:
+            printf("HID_USAGE_CONSUMER_UNDEFINED:           %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_POWER:
+            printf("HID_USAGE_CONSUMER_POWER:               %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_SCAN_PREV_TRACK:
+            printf("HID_USAGE_CONSUMER_SCAN_PREV_TRACK:     %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_EJECT:
+            printf("HID_USAGE_CONSUMER_EJECT:               %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_PLAY_PAUSE:
+            printf("HID_USAGE_CONSUMER_PLAY_PAUSE:          %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_VOL_INC:
+            printf("HID_USAGE_CONSUMER_VOL_INC:             %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_VOL_DEC:
+            printf("HID_USAGE_CONSUMER_VOL_DEC:             %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_MUTE:
+            printf("HID_USAGE_CONSUMER_MUTE:                %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_AL_KEY_LAYOUT:
+            printf("HID_USAGE_CONSUMER_AL_KEY_LAYOUT:       %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_AL_SCREEN_SAVER:
+            printf("HID_USAGE_CONSUMER_AL_SCREEN_SAVER:     %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_AC_SEARCH:
+            printf("HID_USAGE_CONSUMER_AC_SEARCH:           %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_AC_HOME:
+            printf("HID_USAGE_CONSUMER_AC_HOME:             %ld\n", value);
+            break;
+
+        case HID_USAGE_CONSUMER_AC_PAN:
+            printf("HID_USAGE_CONSUMER_AC_PAN:              %ld\n", value);
+            break;
+
+        default:
+            printf("consumer page:                          0x%x = %ld\n", usage, value);
+            break;
+    }
 }
--- source/zc95/Bluetooth/CBluetoothRemote.h
+++ source/zc95/Bluetooth/CBluetoothRemote.h
@@ -12,14 +12,19 @@
 
 // MC = Momentary Control: a basic push button (value expected to be 1 or 0)
 // DV = Dynamic Value: "A Dynamic Value is an n-bit field that contains a value associated with a control"
+// LC = Linear Control: "In many cases, a control of a linear value is implemented as a pair of increment/decrement buttons, a jog wheel, or a linear control such as a knob or a slide"
 
 #define HID_USAGE_PAGE_GENERIC_DESKTOP  0x01
 #define HID_USAGE_PAGE_BUTTON           0x09
+#define HID_USAGE_PAGE_CONSUMER         0x0C
 #define HID_USAGE_PAGE_DIGITIZER        0x0D
 
+
 // PAGE_GENERIC_DESKTOP
-#define HID_USAGE_GENERIC_DESKTOP_X 0x30
-#define HID_USAGE_GENERIC_DESKTOP_Y 0x31
+#define HID_USAGE_GENERIC_DESKTOP_X     0x30
+#define HID_USAGE_GENERIC_DESKTOP_Y     0x31
+#define HID_USAGE_GENERIC_DESKTOP_WHEEL 0x38 // Why would a BT button use this!? but some do! (and always pass the value as 0)
+
 
 // PAGE_BUTTON
 #define HID_USAGE_BUTTON_NONE       0x00
@@ -32,6 +37,22 @@
 #define HID_USAGE_DIGITIZER_CONTACT_IDENTIFIER  0x51 // (DV) "An identifier associated with a contact."
 #define HID_USAGE_DIGITIZER_CONTACT_COUNT       0x54 // (DV) "The current number of contacts the digitizer detects and is reporting."
 
+// PAGE_CONSUMER
+// I've seen phone/shutter remotes that send all of these, and some point I just gave up commenting them.
+// Most are always sent with 0 value, so seem pretty pointless
+#define HID_USAGE_CONSUMER_UNDEFINED        0x00
+#define HID_USAGE_CONSUMER_POWER            0x30 // "Controls the application-specific power state"
+#define HID_USAGE_CONSUMER_SCAN_PREV_TRACK  0xB6 // ""
+#define HID_USAGE_CONSUMER_EJECT            0xB8
+#define HID_USAGE_CONSUMER_PLAY_PAUSE       0xCD
+#define HID_USAGE_CONSUMER_VOL_INC          0xE9  // Common shutter button
+#define HID_USAGE_CONSUMER_VOL_DEC          0xEA 
+#define HID_USAGE_CONSUMER_MUTE             0xE2  // ""
+#define HID_USAGE_CONSUMER_AL_KEY_LAYOUT    0x1AE // "Launch Keyboard Layout Management application"
+#define HID_USAGE_CONSUMER_AL_SCREEN_SAVER  0x1B1 
+#define HID_USAGE_CONSUMER_AC_SEARCH        0x221
+#define HID_USAGE_CONSUMER_AC_HOME          0x223
+#define HID_USAGE_CONSUMER_AC_PAN           0x238 // (LC) "Set the horizontal offset of the display in the document."     
 
 class CBluetoothRemote
 {
@@ -46,6 +67,7 @@ class CBluetoothRemote
         
         enum keypress_t
         {
+            KEY_NONE,
             KEY_UP,
             KEY_DOWN,
             KEY_LEFT,
@@ -61,17 +83,47 @@ class CBluetoothRemote
         void end_of_input();
 
     private:
-        int32_t _last_x;
+   /*     int32_t _last_x;
         int32_t _last_y;
         int32_t _current_x;
         int32_t _current_y;
+    */
         direction_t _direction;
-        bool _pressed;
+        
+        struct dimension_t
+        {
+            uint8_t received_count; // how many received since last movement started event
+            int32_t initial_val;    // before movement started event
+            int32_t prev_val;       // 2nd most recent value received
+            int32_t most_recent;    // most recent value received
+        };
+        
+        bool _movement_started;
+        dimension_t _x;
+        dimension_t _y;
+
 
         void report_keypress();
         void process_desktop_page(uint16_t usage, int32_t value);
         void process_button_page(uint16_t usage, int32_t value);
-        void process_digitizer_page(uint16_t usage, int32_t value);        
+        void process_digitizer_page(uint16_t usage, int32_t value);
+        void process_consumer_page(uint16_t usage, int32_t value);
+
+        bool is_movement_start(uint16_t usage_page, uint16_t usage, int32_t value);
+        bool is_movement_end  (uint16_t usage_page, uint16_t usage, int32_t value);
+        bool is_movement_event(uint16_t usage_page, uint16_t usage);
+        
+        void reset_dimension(dimension_t &dimension);
+
+        keypress_t get_last_button_pressed();
+
+
+        // debug output functions
+        void print_keypress(keypress_t key);
+        void print_desktop_page(uint16_t usage, int32_t value);
+        void print_button_page(uint16_t usage, int32_t value);
+        void print_digitizer_page(uint16_t usage, int32_t value);
+        void print_consumer_page(uint16_t usage, int32_t value);
 };
 
 #endif
