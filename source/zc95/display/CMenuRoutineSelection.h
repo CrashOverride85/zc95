6ffc3e4d9c7128eed50fb742f8171f009548cc91
CrashOverride85 <crashoverride85@outlook.com>
2024-11-03 12:25:49 +0000

    only populate the pattern list when first displaying it, don't regenerate it from scratch when returning

--- source/zc95/display/CMenuRoutineSelection.h
+++ source/zc95/display/CMenuRoutineSelection.h
@@ -50,6 +50,7 @@ class CMenuRoutineSelection : public CMenu
 
     private:
         bool is_audio_routine(routine_conf conf);
+        void populate_routine_list();
         COptionsList *_routine_display_list = NULL;
         struct display_area _area;
         CDisplay* _display;
